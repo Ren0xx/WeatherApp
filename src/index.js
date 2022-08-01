a6d8793c7c607e061dfdc163a67d8f026e1bde06
Adam Załęski <adamzaleski001@wp.pl>
2022-08-01 16:17:36 +0200

    Created frontend for the site

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "../src/style.scss";
+
+// VANTA.CLOUDS2({
+//     el: "body",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     texturePath: "../src/noise.png",
+//     speed: 0.70,
+// })

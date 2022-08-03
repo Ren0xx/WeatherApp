160c0a4603a589652a47ffaa9cd5a25d772ff210
Adam Załęski <adamzaleski001@wp.pl>
2022-08-03 20:29:00 +0200

    Finished project

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # WeatherApp
+
+### This app lets you check the weather in any city on planet Earth.
+
+[Link to the site](https://ren0xx.github.io/WeatherApp/)
+
+
+![obraz](https://user-images.githubusercontent.com/74071928/182681018-5babab5d-b6f9-42bb-985e-254e77242c0e.png)
+
+
+## In this project I used:
+* `VantaJS` - for awesome dynamic background.
+* `Webpack` - as a bundler.
+* `SasS and Bootstrap` - for handling css.
+* `OpenWeatherAPI` - for getting current data about the weather.
+
+![obraz](https://user-images.githubusercontent.com/74071928/182681968-c2a87fe4-9a75-4998-b003-6eca45fe0ee1.png)
--- src/index.js
+++ src/index.js
@@ -1,53 +1,33 @@
 import "../src/style.scss";
 
-const searchBtn = document.getElementById("searchBtn");
-const searchForm = document.getElementById("searchForm");
-const spinner = document.getElementById("loadSpinner");
+const searchedCityName = document.getElementById("searchedCity");
 let temperature = document.getElementById("temperature");
 
+const searchForm = document.getElementById("searchForm");
+const searchBtn = document.getElementById("searchBtn");
+const spinner = document.getElementById("loadSpinner");
+const weatherIcon = document.getElementById("weatherIcon");
+const weatherDesc = document.getElementById("weatherDesc");
+
 const metric = document.getElementById("metric");
-const imperial = document.getElementById("imperial");
-
-const units = ['metric', 'imperial'];
+const units = ["metric", "imperial"];
 let currentUnit = units[0];
-
-// VANTA.CLOUDS2({
-//     el: "body",
-//     mouseControls: true,
-//     touchControls: true,
-//     gyroControls: false,
-//     minHeight: 200.00,
-//     minWidth: 200.00,
-//     scale: 1.00,
-//     texturePath: "../src/images/noise.png",
-//     speed: 0.70,
-// })
 
 async function getData(cityName) {
     try {
-        currentUnit = (metric.checked) ? "metric" : "imperial";
+        currentUnit = metric.checked ? "metric" : "imperial";
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=40e8ac432788c18ba02074a07c6815ab&units=${currentUnit}`,
             { mode: "cors" }
         );
         const data = await response.json();
         return data;
-    } catch (err) {
-    }
+    } catch (err) {}
 }
-
-function processData(data) {
-    const main = data.main;
-    const weather = data.weather;
-    const obj = {
-        main: main,
-        weather: weather,
-    };
-
-    return obj;
+function getIcon(iconID) {
+    return `https://openweathermap.org/img/wn/${iconID}@2x.png`;
 }
-
-function returnWeatherData() {
+function renderWeatherData() {
     searchBtn.addEventListener("click", () => {
         const searchPhrase = searchForm.value.trim();
         if (searchPhrase.length === 0) {
@@ -56,15 +36,34 @@ function returnWeatherData() {
         spinner.hidden = false;
         getData(searchPhrase)
             .then((data) => {
-                console.log(processData(data));
+                weatherIcon.src = getIcon(data.weather[0].icon);
+                searchedCityName.textContent = `${data.name}, ${data.sys.country}`;
+                const currentSign = currentUnit === "metric" ? "°C" : "°F";
+                temperature.textContent = `${Math.round(
+                    data.main.temp
+                )}${currentSign}`;
+
+                weatherDesc.textContent = data.weather[0].description;
                 spinner.hidden = true;
-                temperature.textContent = data.main.temp;
             })
             .catch(() => {
                 spinner.hidden = true;
                 temperature.textContent = "Something went wrong...";
+                weatherIcon.src = "../src/images/face-frown-regular.svg";
             });
     });
 }
 
-returnWeatherData();
+renderWeatherData();
+
+VANTA.CLOUDS2({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    texturePath: "../src/images/noise.png",
+    speed: 0.7,
+});
